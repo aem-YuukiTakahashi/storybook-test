eb408b3b20c51ed2692d50daac53f61ea6165390
aem-YuukiTakahashi <y.takahashi@axelentermedia.jp>
2024-09-06 10:54:32 +0900

    feat:test

--- pages/index.vue
+++ pages/index.vue
@@ -10,16 +10,33 @@
 
 
   div
-    img(src='/images/img1.png')
+    img(:src='imgList[0]' alt="img1")
 
   div
-    a.dl-btn(href='/images/img1.png' download) IMG Download
-
+    a.dl-btn(:href='imgList[0]' download='img1.png') Download
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgList: [
+        "./images/img1.png",
+      ]
+    }
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  methods: {
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 .dl-btn {
-  display: block;
+  display: inline-block;
   padding: 15px 20px;
   background-color: #fff;
   border: 1px solid #000;
