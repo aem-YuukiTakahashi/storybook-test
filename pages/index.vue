81cd66622bc2811d045bab4ba7e0b2185f9fef30
aem-YuukiTakahashi <y.takahashi@axelentermedia.jp>
2024-09-06 10:32:11 +0900

    feat:test

--- pages/index.vue
+++ pages/index.vue
@@ -7,4 +7,22 @@
       :disabled="false"
     )
       | Button
+
+
+  div
+    img(src='/images/img1.png')
+
+  div
+    a.dl-btn(href='/images/img1.png' download) IMG Download
+
 </template>
+
+<style lang="scss" scoped>
+.dl-btn {
+  display: block;
+  padding: 15px 20px;
+  background-color: #fff;
+  border: 1px solid #000;
+  color: #000;
+}
+</style>
